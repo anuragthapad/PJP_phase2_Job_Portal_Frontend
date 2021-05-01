3cfb9f573e7b55abbbd5b7c016df5a5f650a60ed
Kishor Chand <kishorec15@gmail.com>
2021-05-01 15:58:53 +0530

    User SignUp first page(optional)

---
+++ PJP/javascript/mySignUP.js
@@ -0,0 +1,88 @@
+function startWithU(targ) {
+	if ((targ.startsWith("u", 0) || targ.startsWith("U", 0)) == true) {
+		return true;
+	}
+	else return false;
+
+};
+
+function startWithA(targ) {
+	if ((targ.startsWith("a", 0) || targ.startsWith("A", 0)) == true) {
+		return true;
+	}
+	else return false;
+
+};
+function validateform(form) {
+	var temp = document.getElementById("user_id").value;
+	if (temp.length < 8) {
+		document.getElementById("user_val").innerHTML = "Error : Enter 8 character User ID.";
+		document.getElementById("SignUP_form").reset();
+		return false;
+
+	}
+	var sU = startWithU(temp);
+	var sA = startWithA(temp);
+	if ((sU || sA) == false) {
+		document.getElementById("user_val").innerHTML = "Error : User ID must start with U or A.";
+		document.getElementById("SignUP_form").reset();
+		return false;
+	}
+	
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+	if(!RegExp.escape) {
+		RegExp.escape = function(s) {
+		  return String(s).replace(/[\\^$*+?.()|[\]{}]/g, '\\$&');
+		};
+	  }
+
+    // JavaScript form validation
+
+    var checkPassword = function(str)
+    {
+      var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
+      return re.test(str);
+    };
+
+	var supports_input_validity = function()
+    {
+      var i = document.createElement("input");
+      return "setCustomValidity" in i;
+    }
+    if(supports_input_validity()) {
+		/*var usernameInput = document.getElementById("field_username");*/
+		/*usernameInput.setCustomValidity(usernameInput.title);*/
+  
+		var pwd1Input = document.getElementById("myPwd1");
+		pwd1Input.setCustomValidity(pwd1Input.title);
+  
+		var pwd2Input = document.getElementById("myPwd2");
+  
+		// input key handlers
+  
+		/*usernameInput.addEventListener("keyup", function(e) {
+		  usernameInput.setCustomValidity(this.validity.patternMismatch ? usernameInput.title : "");
+		}, false);*/
+  
+		pwd1Input.addEventListener("keyup", function(e) {
+		  this.setCustomValidity(this.validity.patternMismatch ? pwd1Input.title : "");
+		  if(this.checkValidity()) {
+			pwd2Input.pattern = RegExp.escape(this.value);
+			pwd2Input.setCustomValidity(pwd2Input.title);
+		  } else {
+			pwd2Input.pattern = this.pattern;
+			pwd2Input.setCustomValidity("");
+		  }
+		}, false);
+  
+		pwd2Input.addEventListener("keyup", function(e) {
+		  this.setCustomValidity(this.validity.patternMismatch ? pwd2Input.title : "");
+		}, false);
+  
+	  }
+  
+	}, false);
+
+
