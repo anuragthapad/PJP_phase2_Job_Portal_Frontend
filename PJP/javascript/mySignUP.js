f02faae2896633ee4dec73a8fa344c7d93919b1d
Kishor Chand <kishorec15@gmail.com>
2021-05-01 16:12:15 +0530

    User SignUp adjustment to fit the box properly on the screen.

--- PJP/javascript/mySignUP.js
+++ PJP/javascript/mySignUP.js
@@ -28,61 +28,51 @@ function validateform(form) {
 		document.getElementById("SignUP_form").reset();
 		return false;
 	}
-	
+
 };
 
-document.addEventListener("DOMContentLoaded", function() {
-	if(!RegExp.escape) {
-		RegExp.escape = function(s) {
-		  return String(s).replace(/[\\^$*+?.()|[\]{}]/g, '\\$&');
+document.addEventListener("DOMContentLoaded", function () {
+	if (!RegExp.escape) {
+		RegExp.escape = function (s) {
+			return String(s).replace(/[\\^$*+?.()|[\]{}]/g, '\\$&');
 		};
-	  }
+	}
 
-    // JavaScript form validation
 
-    var checkPassword = function(str)
-    {
-      var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
-      return re.test(str);
-    };
+	var checkPassword = function (str) {
+		var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
+		return re.test(str);
+	};
 
-	var supports_input_validity = function()
-    {
-      var i = document.createElement("input");
-      return "setCustomValidity" in i;
-    }
-    if(supports_input_validity()) {
-		/*var usernameInput = document.getElementById("field_username");*/
-		/*usernameInput.setCustomValidity(usernameInput.title);*/
-  
+	var supports_input_validity = function () {
+		var i = document.createElement("input");
+		return "setCustomValidity" in i;
+	}
+	if (supports_input_validity()) {
+
+
 		var pwd1Input = document.getElementById("myPwd1");
 		pwd1Input.setCustomValidity(pwd1Input.title);
-  
+
 		var pwd2Input = document.getElementById("myPwd2");
-  
-		// input key handlers
-  
-		/*usernameInput.addEventListener("keyup", function(e) {
-		  usernameInput.setCustomValidity(this.validity.patternMismatch ? usernameInput.title : "");
-		}, false);*/
-  
-		pwd1Input.addEventListener("keyup", function(e) {
-		  this.setCustomValidity(this.validity.patternMismatch ? pwd1Input.title : "");
-		  if(this.checkValidity()) {
-			pwd2Input.pattern = RegExp.escape(this.value);
-			pwd2Input.setCustomValidity(pwd2Input.title);
-		  } else {
-			pwd2Input.pattern = this.pattern;
-			pwd2Input.setCustomValidity("");
-		  }
+
+		pwd1Input.addEventListener("keyup", function (e) {
+			this.setCustomValidity(this.validity.patternMismatch ? pwd1Input.title : "");
+			if (this.checkValidity()) {
+				pwd2Input.pattern = RegExp.escape(this.value);
+				pwd2Input.setCustomValidity(pwd2Input.title);
+			} else {
+				pwd2Input.pattern = this.pattern;
+				pwd2Input.setCustomValidity("");
+			}
 		}, false);
-  
-		pwd2Input.addEventListener("keyup", function(e) {
-		  this.setCustomValidity(this.validity.patternMismatch ? pwd2Input.title : "");
+
+		pwd2Input.addEventListener("keyup", function (e) {
+			this.setCustomValidity(this.validity.patternMismatch ? pwd2Input.title : "");
 		}, false);
-  
-	  }
-  
-	}, false);
+
+	}
+
+}, false);
 
 
